fc6b9a01be052570da185ae925e55a1b70559c86
Will2017001 <33051915+Will2017001@users.noreply.github.com>
2018-09-18 11:06:51 +0800

    Add files via upload

---
+++ HelloMike.java
@@ -0,0 +1,10 @@
+1	import java.util.Scanner;
+2	class HelloMike{
+3		public static void main(String[] args){
+4			Scanner sc = new Scanner(System.in);
+5			String name = sc.next();
+6			System.out.println("Hello," + name );
+name = "Hello";
+name = name + "," + "Mike";
+7		}
+8	}
