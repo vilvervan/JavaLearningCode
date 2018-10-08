1782faf96a2f15c6a3ccbedb4442be5e6dd2e88d
Will2017001 <33051915+Will2017001@users.noreply.github.com>
2018-10-08 11:47:55 +0800

    Add files via upload

---
+++ ComplexNumber.java
@@ -0,0 +1,18 @@
+class ComplexNumber{
+	private double re;
+	private double im;
+
+	public double modulus(){
+		return Math.hypot(re,im);
+	}
+				
+	public String toString() {
+		if (im == 0) 
+			return re + "";
+		if (re == 0) 
+			return im + "i";
+		if (im <  0) 
+			return re + " - " + (-im) + "i";
+		return re + " + " + im + "i";
+	}	
+}
